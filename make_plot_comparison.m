8165f9af761c1d925d998a2a9f067d57fdef9000
Jun Feng <91000496+JUNFeng96@users.noreply.github.com>
2021-11-04 14:54:43 +0800

    Delete make_plot_comparison.m

--- make_plot_comparison.m
+++
@@ -1,8 +0,0 @@
-% Makes the plot of extracted pulse(FP) and original ground motion(velocity)  
-
-function [] = make_plot_comparison(FP,velocity,t)
- figure('Name', 'comparison');
- plot(t,FP,'r','LineWidth',3);
- hold on;
- plot(t,velocity,'k');                  
-end
