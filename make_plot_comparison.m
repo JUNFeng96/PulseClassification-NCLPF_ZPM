7ff11d157561fc48c0036bc1b75ff5d273d4b7c6
Jun Feng <91000496+JUNFeng96@users.noreply.github.com>
2021-11-04 15:29:25 +0800

    Update make_plot_comparison.m

--- make_plot_comparison.m
+++ make_plot_comparison.m
@@ -1,4 +1,4 @@
-% Makes the plot of extracted pulse(FP) and original ground motion(velocity)  
+% Makes the plot of extracted pulse(FP) and original ground motion(velocity)  [JUN Feng,2020]
 
 function [] = make_plot_comparison(FP,velocity,t)
  figure('Name', 'comparison');
